3a25056f91caa4424a9771a44f5681bce94880e2
Koba1 <LOP000000000000000@gmail.com>
2015-09-22 22:08:59 +0300

    creare Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 public class Calculate {
 public static void main(String[] arg){
 System.out.println("Calculate...");
+int first = Integer.valueof(arg[0]);
+int second = Integer.valueof(arg[1]);
+int summ = first + second;
+System.out.println("sum " + summ);
 }
 }
